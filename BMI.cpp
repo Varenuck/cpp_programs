3d1fa8e16502db566c5c82f714862beb4ff55338
Dmytro Obrusnik <therardd@gmail.com>
2019-07-04 21:23:14 +0300

    some changes

--- BMI.cpp
+++ BMI.cpp
@@ -9,15 +9,15 @@ int main()
 {
 	setlocale(0, "Russian");
 	double h, m, BMI;
-	cout << "введiть свiй рiст в метрах:\n" << endl;
+	cout << "Введiть свiй рiст в метрах:\n" << endl;
 	cin >> h;
-	cout << "введiть свою масу в кг:\n" << endl;
+	cout << "Введiть свою масу в кг:\n" << endl;
 	cin >> m;
 	BMI = m / (h*h);
 	cout << "Ваш BMI: " << BMI << endl;
 	if (BMI <= 18.5)
 	{
-		cout << "Недостатьня маса\n" << endl;
+		cout << "Недостатьня Маса\n" << endl;
 	}
 	else
 		if (BMI <= 25)
